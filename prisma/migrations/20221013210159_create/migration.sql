43b9c264f07330951c0c047708c3d0ce77e2c6e2
fabiorodriguesroque <fabioroque9@gmail.com>
2022-10-13 22:15:08 +0100

    improve register +page.server

--- prisma/migrations/20221013210159_create/migration.sql
+++ prisma/migrations/20221013210159_create/migration.sql
@@ -3,8 +3,10 @@ CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
-    `refres_token` VARCHAR(191) NOT NULL,
+    `uuid` VARCHAR(191) NOT NULL,
+    `refresh_token` VARCHAR(191) NOT NULL,
 
     UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `User_uuid_key`(`uuid`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- src/routes/(auth)/register/+page.server.ts
+++ src/routes/(auth)/register/+page.server.ts
@@ -1,10 +1,12 @@
 import type { Actions } from './$types'; 
 import { db } from '$lib/database';
-import { invalid } from '@sveltejs/kit';
+import { invalid, redirect } from '@sveltejs/kit';
 import { hashSync } from 'bcryptjs';
+import { randomUUID } from 'crypto';
+import jsonwebtoken from 'jsonwebtoken';
 
 export const actions: Actions = {
-    register: async ({ request }) => {
+    register: async ({ cookies, request }) => {
         const data = await request.formData();
         const email = data.get('email');
         const password = data.get('password');
@@ -12,21 +14,39 @@ export const actions: Actions = {
 
         if (password === password_confirmation) {
             createUser(String(email), String(password));
+
+            /**
+             * TO-DO 
+             * remove duplicated code in login +page.server.ts
+             */
+            const jwt = jsonwebtoken.sign({username: email}, import.meta.env.VITE_JWT_PRIVATE_KEY, { expiresIn: '3m' });
+        
+            cookies.set('sveltekit_auth_app', String(jwt), {
+                path: '/',
+                httpOnly: true,
+                sameSite: 'strict',
+                secure: false,
+                maxAge: 60 * 60 * 35 * 30
+            });
         } else {
             return invalid(400, {password, passwordsNotMatch: true});
-        }   
+        }
+        
+        throw redirect(302, '/dashboard');
 
     }
 }
 
 async function createUser(email: string, password: string) {
-    const passwordHash = hashSync(password, 14);
-
+    const passwordHash: string = hashSync(password, 14);
+    const uuid = randomUUID();
+    const refresh_token = randomUUID();
     await db.user.create({
         data: {
             email: email,
             password: passwordHash, 
-            refresh_token: 'eqweqweqw'
+            uuid: uuid,
+            refresh_token: refresh_token,
         }
     }) 
 }
